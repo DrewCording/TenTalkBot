169fa0a862aa69a354f276ddda68306fab8ddf5b
root <root@localhost>
2021-02-23 00:33:40 +0000

    Moved some variables to .env

--- disc_to_cc.py
+++ disc_to_cc.py
@@ -1,5 +1,5 @@
 import os
-import time 
+import time
 import discord
 import random
 from dotenv import load_dotenv
@@ -11,19 +11,24 @@ client = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
-	print('Disc_to_CC started on bot {0.user}'.format(client))
-	
+        print('Disc_to_CC started on bot {0.user}'.format(client))
+
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-		
-	if message.channel.id == 813255331840524319:
-		text = (message.author.nick + ": " + message.content)
-                await message.delete()
-		intv = "0.0" + str(random.randrange(01, 50, 1))
-		pyautogui.hotkey'enter')
-		pyautogui.write(text, interval = intv)
-		pyautogui.hotkey('enter')
+        if message.author == client.user:
+                return
 
+        if message.channel.id == int(os.getenv('channel')):
+                if message.author.nick:
+                    text = (message.author.nick + ": " + message.content)
+                else:
+                    text = (message.author.name + ": " + message.content)
+				
+                await message.delete()
+                intv = "0.0" + str(random.randrange(50, 99, 1))
+                pyautogui.hotkey('enter')
+                pyautogui.write(text, interval = intv)
+                pyautogui.hotkey('enter')
+				
 client.run(os.getenv('TOKEN'))
+
