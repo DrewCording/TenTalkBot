b9e702a9b589bfc960902c52f6e2f577c32b3fa8
root <root@localhost>
2021-02-23 00:36:00 +0000

    Added function to relog when force logged out

--- keep_alive.py
+++ keep_alive.py
@@ -2,11 +2,27 @@ import os
 import time
 import random
 import pyautogui
+from dotenv import load_dotenv
+from python_imagesearch.imagesearch import imagesearch
+
+load_dotenv()
 
 while 1:
-	intv1 = random.randrange(60, 120, 1)
-	time.sleep(intv1)
-	intv2 = random.randrange(60, 120, 1)
-	time.sleep(intv2)
-	print("sleep time was " + str(intv1) + " + " + str(intv2))
-	pyautogui.click()
+	pos = imagesearch("./login.png")
+	
+	if pos[0] != -1:
+		pyautogui.moveTo(820, 670, 2, pyautogui.easeOutQuad)
+		pyautogui.click()
+		pyautogui.write(os.getenv('password'))
+		pyautogui.hotkey('enter')
+		pyautogui.moveTo(820, 690, 1, pyautogui.easeOutQuad)
+		time.sleep(10)
+		pyautogui.click()
+		
+	else:
+		intv1 = random.randrange(60, 120, 1)
+		time.sleep(intv1)
+		intv2 = random.randrange(60, 120, 1)
+		time.sleep(intv2)
+		pyautogui.moveTo(1110, 1030, 2, pyautogui.easeOutQuad)
+		pyautogui.click()
