a7a1840144013ee298fadaf4bca1d903e409a970
root <root@localhost>
2021-02-24 20:40:37 +0000

    Added mod check

--- giverank.py
+++ giverank.py
@@ -20,6 +20,7 @@ async def on_ready():
 
 
 @client.command()
+@commands.has_permissions(manage_roles=True)
 async def giverank(ctx, user: discord.Member, rsn):
     if message.channel.id == int(os.getenv('channel')):
         return
