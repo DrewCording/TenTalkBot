61e5135b99bcfde0669f3c5d87f39111b9bb692d
A. Cording <DrewCording@gmail.com>
2021-02-24 09:53:56 -0500

    Added char limit

--- disc_to_cc.py
+++ disc_to_cc.py
@@ -16,19 +16,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-        if message.author == client.user:
-                return
+	if message.author == client.user:
+		return
 
-        if message.channel.id == int(os.getenv('channel')):
-                if message.author.nick:
-                    text = (message.author.nick + ": " + message.content)
-                else:
-                    text = (message.author.name + ": " + message.content)
+	if message.channel.id == int(os.getenv('channel')):
+		if message.author.nick:
+			text = ("/[" + message.author.nick + "] " + message.content)
+		else:
+			text = ("/[" + message.author.name + "] " + message.content)
 				
-                await message.delete()
-                intv = "0.0" + str(random.randrange(50, 99, 1))
-                pyautogui.write('/')
-                pyautogui.write(text, interval = intv)
-                pyautogui.hotkey('enter')
+		if len(text) > 80:
+			text = text[:80]
+			user = message.author
+			await user.send("Warning: Your last message <#" + os.getenv('channel') + "> was too long. Messages are limited to 80 characters including your name")
+				
+		await message.delete()
+		intv = "0.0" + str(random.randrange(50, 99, 1))
+		pyautogui.write(text, interval = intv)
+		pyautogui.hotkey('enter')
 				
 client.run(os.getenv('TOKEN'))
