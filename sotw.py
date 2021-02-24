db3c3729aea96ff812c733bd3479607bba98b43a
root <root@localhost>
2021-02-24 19:15:58 +0000

    Added \!sotw functionality

---
+++ sotw.py
@@ -0,0 +1,33 @@
+#!/bin/python3 -u
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+client = commands.Bot(command_prefix='!')
+
+@client.event
+async def on_ready():
+    print('!sotw started on bot {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    content = message.content
+    if content.startswith("!sotw"):
+
+        sotw_file = open("sotw.msg", "r")
+        sotw_msg = sotw_file.read()
+
+        if message.author == client.user:
+            if message.channel.id == int(os.getenv('botchan')):
+                cc_chan = client.get_channel(int(os.getenv('channel')))
+                await cc_chan.send(str('*' + sotw_msg))
+        else:
+
+            await message.channel.send(str(sotw_msg))
+    else:
+        return
+client.run(os.getenv('TOKEN'))
+
