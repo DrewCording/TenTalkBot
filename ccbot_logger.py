2d2f27384c38aaf6c7270ddc75425bb17b0d7e36
root <root@localhost>
2021-02-25 22:10:25 +0000

    Added support to generate metrics for logins and messages

--- ccbot_logger.py
+++ ccbot_logger.py
@@ -17,11 +17,40 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    else:
-        if message.channel.id == int(os.getenv('channel')):
+    if message.channel.id == int(os.getenv('channel')):
+        if message.author == client.user:
+            content = str(message.content)
+            if content.startswith('*'):
+                return
+            
+            rsn_ptrn = "] (.*?): "
+            log_ptrn = "] (.*?) has joined"
+            emt_ptrn = "<:(.*?)>"
+    
+            rsn = re.search(rsn_ptrn, content)
+            log = re.search(log_ptrn, content)
+            emt = re.search(emt_ptrn, content)
+    
+            if log:
+                cc_logins = open("cc_logins.log", "a")
+                cc_logins.write(str(str(log.group(1))+ "\n"))
+                cc_logins.close()
+
+            if rsn:
+                if emt:
+                    emt_full = str("<:" + str(emt.group(1)) + ">")
+                    rsn = str(rsn.group(1)).replace(emt_full, "")
+                else:
+                    rsn = rsn.group(1)
+
+                cc_messages = open("cc_messages.log", "a")
+                cc_messages.write(str(rsn + "\n"))
+                cc_messages.close()
+
+        else:
             print(message.author,":",message.content)
+    else:
+        return
 
 client.run(os.getenv('TOKEN'))
 
