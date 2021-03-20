26755cd347b97a28e1f653fb98afdb8ffaea8e1c
Drew <drew@nothanks.com>
2021-03-20 12:54:49 +0000

    Added line to delete old jars

--- runelite_compile.sh
+++ runelite_compile.sh
@@ -3,4 +3,5 @@
 logdirect=$PWD
 cd /runelite/runelite/
 git pull origin master &>> $logdirect/runelite_compile.log &
+rm /runelite/runelite/runelite-client/target/*.jar
 mvn install -Dskiptests &>> $logdirect/runelite_compile.log &
