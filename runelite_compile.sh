2adc4026bdc39b88e6125c2741bdbf13181163b8
Drew <drew@nothanks.com>
2021-05-12 18:20:46 +0000

    Added test skipping

--- runelite_compile.sh
+++ runelite_compile.sh
@@ -4,4 +4,4 @@ logdirect=$PWD
 cd /runelite/runelite/
 git pull origin master &>> $logdirect/runelite_compile.log &
 rm /runelite/runelite/runelite-client/target/*.jar
-mvn install &>> $logdirect/runelite_compile.log &
+mvn install -DskipTests &>> $logdirect/runelite_compile.log &
