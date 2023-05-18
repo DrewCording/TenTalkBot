7ed00dab6f81af840e88fec9f0045545e5a8e85f
Drew <drew@nothanks.com>
2023-05-18 18:51:31 +0000

    Removed note about free masses in raids

--- super_bot.py
+++ super_bot.py
@@ -938,14 +938,14 @@ async def on_message(message):
         if message.author == client.user:
             if message.channel.id == int(os.getenv('botchan')):
                 cc_chan = client.get_channel(int(os.getenv('channel')))
-                await cc_chan.send('*Use this in Discord for an invite to Level 3 Raids server. Free masses weekly')
+                await cc_chan.send('*Use this in Discord for an invite to Level 3 Raids server. Learn how to get purps on level 3!')
         else:
             if message.author.nick:
                 name = message.author.nick
             else:
                 name = message.author.name
 
-            await message.channel.send("If you are interested in learning CoX Raids on a Level 3 or 10HP, check out the Level 3 Raids Discord Server. Free mass raids every week. Invite link: https://discord.gg/wVSkAABhdr")
+            await message.channel.send("If you are interested in learning CoX Raids on a Level 3 or 10HP, check out the Level 3 Raids Discord Server. Invite link: https://discord.gg/wVSkAABhdr")
     else:
         return
 
